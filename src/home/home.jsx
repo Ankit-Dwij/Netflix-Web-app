1fc42f60a8fb6b8f13f6c3dd342715dd3b295408
Ankit Dwij <ankitdwiz010@gmail.com>
2022-05-13 14:04:00 +0530

    material-ui React

---
+++ src/home/home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar/navbar";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
